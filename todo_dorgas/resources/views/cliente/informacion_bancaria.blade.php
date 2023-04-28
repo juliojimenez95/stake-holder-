4eb34ea0451aea4cd768520ffe79e1d21e34889d
Julio Jimenez <juliojimenegarcia@gmail.com>
2023-04-28 09:57:28 -0500

    registo v2

--- todo_dorgas/resources/views/cliente/informacion_bancaria.blade.php
+++ todo_dorgas/resources/views/cliente/informacion_bancaria.blade.php
@@ -6,129 +6,153 @@
   <title>Bienvenido</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/bancaria.css') }}">
 </head>
 <body>
   <div class="container-fluid">
-    <div class="row">
+    <div class="row mb-4">
       <div class="col-md-12">
         <div class="text-center">
           <img src="{{ asset('images/logo_t2.png') }}" class="my-4 img-fluid">
-          <h1 class="text-primary">Información Bancaria</h1>
-          <hr class="underline">
+          <h1 class="text-primary h1_tit">Información Bancaria</h1>
+          <hr class="underline under_S">
         </div>
       </div>
-    </div> 
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card bg-light mt-4">
-          <div class="card-body">
-          <form action="">
-          <div class="row">
+    </div>
+    <div class="container">
+        <div class="row">
             <div class="col-md-6">
-              <div class="form-group">
-                <div class="row">
-                  <div class="col text-center">
-                    <span class="mr-2">Activo y pasivo total a 31 <br> de diciembre del año anterior. </span>
-                  </div>
+                <div class="mb-4">
+                    <button class="btn btn-primary btn_1">Banco 1</button>
                 </div>
-              </div>
-              <br> 
-              <div class="form-group">
                 <div class="row">
-                    <div class="col text-center">
-                      <span class="mr-2">Respuesta </span>
+                    <div class="col-md-6">
+                        <div>
+                            <p class="title-ent">Nombre de la Entidad Bancaria<span>*</span></p>
+                            <div class="div-filled">
+                                <div>
+                                    <select size="5" multiple class="select_cus">
+                                        <option value="opcion1">Opción 1</option>
+                                        <option value="opcion2">Opción 2</option>
+                                        <option value="opcion3">Opción 3</option>
+                                        <option value="opcion4">Opción 4</option>
+                                        <option value="opcion5">Opción 5</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                      <div class="col text-center">
-                        <p class="mr-2">Activo </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
+                    <div class="col-md-6">
+                        <form action="">
+                            <div class="mb-5">
+                                <p class="title-ent">Tipo de Cuenta<span>*</span></p>
+                                <div class="div-filled">
+                                    <select size="2" multiple class="select_cus">
+                                        <option value="opcion1">AHORROS</option>
+                                        <option value="opcion2">CORRIENTE</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Número de cuenta<span>*</span></label>
+                                <input type="text" class="form-control input_custom">
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Ciudad<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Departamento<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">País<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                <div class="col text-center">
-                        <p class="mr-2">Pasivo </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                <div class="col text-center">
-                        <p class="mr-2">Patrimonio </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
-                </div>
-              </div>
             </div>
-            
             <div class="col-md-6">
-              <div class="form-group">
-                <div class="row">
-                  <div class="col text-center">
-                    <span class="mr-2">Activo y pasivo total a 31 <br> de diciembre del año anterior. </span>
-                  </div>
+                <div class="mb-4">
+                    <button class="btn btn-primary btn_1">(Opcional) Banco 2</button>
                 </div>
-              </div>
-              <br> 
-              <div class="form-group">
                 <div class="row">
-                    <div class="col text-center">
-                      <span class="mr-2">Respuesta </span>
+                    <div class="col-md-6">
+                        <div>
+                            <p class="title-ent">Nombre de la Entidad Bancaria</p>
+                            <div class="div-filled">
+                                <div>
+                                    <select size="5" multiple class="select_cus">
+                                        <option value="opcion1">Opción 1</option>
+                                        <option value="opcion2">Opción 2</option>
+                                        <option value="opcion3">Opción 3</option>
+                                        <option value="opcion4">Opción 4</option>
+                                        <option value="opcion5">Opción 5</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                      <div class="col text-center">
-                        <p class="mr-2">Activo </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
+                    <div class="col-md-6">
+                        <form action="">
+                            <div class="mb-5">
+                                <p class="title-ent">Tipo de Cuenta<span>*</span></p>
+                                <div class="div-filled">
+                                    <select size="2" multiple class="select_cus">
+                                        <option value="opcion1">AHORROS</option>
+                                        <option value="opcion2">CORRIENTE</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Número de cuenta<span>*</span></label>
+                                <input type="text" class="form-control input_custom">
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Ciudad<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">Departamento<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group mb-4">
+                                <label for="" class="label_custom">País<span>*</span></label>
+                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                    <option value="">Seleccione una opcion*</option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                <div class="col text-center">
-                        <p class="mr-2">Pasivo </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                <div class="col text-center">
-                        <p class="mr-2">Patrimonio </p>
-                      </div>
-                    
-                      <input type="text" class="form-control col-5" id="campo8" name="campo8">
-                </div>
-              </div>
             </div>
-            
-          </div>
-
-
-         </div>
-
-          <center style="margin-top:10px">
-             <div class="col-md-12">
-                <button type="submit" class="btn btn-primary estilo_boton">continuar</button>
-              </div>
-          </center>
-        </form>
-
         </div>
-      </div>
+        <div class="mb-4">
+            <div class="div_continuar">
+                <button class="btn btn-primary btn_continuar">CONTINUAR</button> <span style="color:blue; font-size: 25px;"><i class="fa-solid fa-arrow-right"></i></span>
+            </div>
+        </div>
     </div>
   </div>
 

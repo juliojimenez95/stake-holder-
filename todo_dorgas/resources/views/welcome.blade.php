446916306c0bf1a3061e29250991347145ecd9c3
Leiton Aricapa <leiton.aripara@sssas.com.co>
2023-05-02 09:09:16 -0500

    updated leiton

--- todo_dorgas/resources/views/welcome.blade.php
+++ todo_dorgas/resources/views/welcome.blade.php
@@ -5,31 +5,66 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Bienvenido</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/wel.css') }}">
 </head>
 <body>
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-12">
+        <div class="abs_cont">
+          <div class="div_fa">
+            <div class="img_fa">
+              <i class="fa-solid fa-user"></i>
+            </div>
+          </div>
+          
+          <div class="div_father">
+            <div class="text_fa">
+              <div class="cultura_img">
+                <img src="{{ asset('images/Cultura-de-Legalidad.png') }}"   class="img-fluid">
+              </div>
+              <p>Conoce nuestra Cultura de Legalidad</p>
+            </div>
+          </div>
+
+          <div class="div_select">
+            <select id="mySelect" multiple size="4">
+              <option value="option1">Políticas de Tratamiento de Datos</option>
+              <option value="option2">Políticas de Tratamiento de información</option>
+              <option value="option3">Politicas SAGRILAFT</option>
+              <option value="option3">Código de Ética y Buen Gobierno</option>
+            </select>
+          </div>
+
+        </div>
         <div class="text-center">
           <img src="{{ asset('images/logo_t2.png') }}"   class=" my-4 img-fluid">
-          <h1 class="text-primary">Bienvenido</h1>
-          <hr class="underline">
-        <strong><p class="text-primary">Seleccione el perfil al que desea ingresar</p></strong>
+          <h1 class="text-primary h1_cus">Bienvenido</h1>
+          <hr class="underline"><p class="text-primary p_cus">Seleccione el perfil al que deseas ingresar</p>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-6" >
+      <div class="col-md-6" style="padding: 0!important;">
         <a href="{{ route('cliente.identificacion') }}" class="w-100">
         <img src="{{ asset('images/cliente.png') }}" alt="Imagen de perfil 1" class="img-responsive my-4 img-fluid w-100 ">
         </a>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="padding: 0!important;">
         <a href="{{ route('cliente.identificacion') }}"  class="w-100">
         <img src="{{ asset('images/proveedor.png') }}" alt="Imagen de perfil 2" class="img-responsive my-4 img-fluid w-100">
         </a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text_link">
+          <a href="">Stakeholders.tododrogas.com.co</a>
+        </div>
       </div>
     </div>
   </div>

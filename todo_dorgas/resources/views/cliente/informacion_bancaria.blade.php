68d8b226a13a43b744f1142f1c72142b4b3868c4
Julio Jimenez <juliojimenegarcia@gmail.com>
2023-05-07 18:37:16 -0500

    info bancaria

--- todo_dorgas/resources/views/cliente/informacion_bancaria.blade.php
+++ todo_dorgas/resources/views/cliente/informacion_bancaria.blade.php
@@ -10,6 +10,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
   <link rel="stylesheet" href="{{ asset('css/bancaria.css') }}">
+  <script  src="https://code.jquery.com/jquery-3.6.3.js"
+            integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+            crossorigin="anonymous"></script>
 
   <link href="https://www.dafontfree.net/embed/ZnJhbmtsaW4tZ290aGljLWJvb2stcmVndWxhciZkYXRhLzMyL2YvMTU0MjI1L0ZyYW5rbGluIEdvdGhpYyBCb29rIFJlZ3VsYXIudHRm" rel="stylesheet" type="text/css"/>
   <link href="https://allfont.net/allfont.css?fonts=franklin-gothic-medium" rel="stylesheet" type="text/css" />
@@ -27,12 +30,15 @@
       </div>
     </div>
     <div class="container">
+      <form action="{{ route('clientes.storeInformacionb',$id) }}" method="POST">
+            @csrf
         <div class="row">
             <div class="col-md-6">
                 <div class="mb-4">
                     <button class="btn btn-primary btn_1">Banco 1</button>
                 </div>
                 <div class="row">
+
                     <div class="col-md-6">
                         <div>
                             <p class="title-ent">Nombre de la Entidad Bancaria<span>*</span></p>
@@ -51,7 +57,7 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <form action="">
+
                             <div class="mb-5">
                                 <p class="title-ent">Tipo de Cuenta<span>*</span></p>
                                 <div class="div-filled">
@@ -68,31 +74,37 @@
                             </div>
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Número de cuenta<span>*</span></label>
-                                <input type="text" class="form-control input_custom">
+                                <input type="text" class="form-control input_custom" name="n_cuenta">
+                                @if ($errors->has('n_cuenta'))
+                                        <p class="text-danger">{{ $errors->first('n_cuenta') }}</p>
+                                    @endif
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Ciudad<span>*</span></label>
-                                <select class="form-control select_custom" name="municipio" id="exampleFormControlSelect1">
+                                <select class="form-control select_custom" name="municipio" id="municipio">
                                     <option value="">Medellin</option>
                                 </select>
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Departamento<span>*</span></label>
-                                <select class="form-control select_custom" name="departamento" id="exampleFormControlSelect1">
-                                    <option value="">Antioquia</option>
-                                </select>
+                                <select  class="form-select select_custom" aria-label="Default select example" id="departamento" name="departamento">
+                                    <option  value="{{old('departamento')}}">Seleccione una departamento...</option>
+
+                                    </select>
+                                    @if ($errors->has('departamento'))
+                                        <p class="text-danger">{{ $errors->first('departamento') }}</p>
+                                    @endif
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">País<span>*</span></label>
-                                <select class="form-control select_custom"  id="exampleFormControlSelect1">
-                                    <option value="">Colombia</option>
+                                <select class="form-control select_custom"  id="pais" name="pais">
+                                    <option value="Colombia">Colombia</option>
                                 </select>
                             </div>
 
-                        </form>
                     </div>
                 </div>
             </div>
@@ -121,7 +133,6 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <form action="">
                             <div class="mb-5">
                                 <p class="title-ent">Tipo de Cuenta</p>
                                 <div class="div-filled">
@@ -139,49 +150,86 @@
                             </div>
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Número de cuenta<span>*</span></label>
-                                <input type="text" class="form-control input_custom">
+                                <input type="text" class="form-control input_custom"name="n_cuenta2">
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Ciudad<span>*</span></label>
-                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                <select class="form-control select_custom" name="municipio2" id="municipio2">
                                     <option value="">Medellin</option>
                                 </select>
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">Departamento<span>*</span></label>
-                                <select class="form-control select_custom" id="exampleFormControlSelect1">
+                                <select class="form-control select_custom" id="departamento2" name="departamento2">
                                     <option value="">Antioquia*</option>
                                 </select>
                             </div>
 
                             <div class="form-group mb-4">
                                 <label for="" class="label_custom">País<span>*</span></label>
-                                <select class="form-control select_custom" id="exampleFormControlSelect1">
-                                    <option value="">Colombia</option>
+                                <select class="form-control select_custom" name="pais2" id="pais2">
+                                    <option value="Colombia">Colombia</option>
                                 </select>
                             </div>
-                        </form>
+
                     </div>
                 </div>
             </div>
         </div>
         <div class="mb-4">
             <div class="div_continuar">
-                <a href="{{ route('home') }}">
                 <button class="btn btn-primary btn_continuar">CONTINUAR</button> <span style="color:blue; font-size: 25px;"><i class="fa-solid fa-arrow-right"></i></span>
-                </a>
             </div>
         </div>
     </div>
+    </form>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper-base.min.js"
     integrity="sha384-EQhgPShYZDmf+OWKvoYf70b91G/J0xgfgvbXhNfP60ZQLPv6du/0h0sU6Ygr19d0"
     crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <script src="{{ asset('js/principal.js') }}"></script>
+
+  <script>
+    $(document).ready(function(){
+    $.ajax({
+    url: "/listarMunicipios",
+    dataType: "json",
+    type: "GET",
+    success: function(response) {
+
+            departamentos_local = response.departamentos;
+            municipios_local = response.municipios;
+            localStorage.setItem('departamentos_local', JSON.stringify(departamentos_local));
+            localStorage.setItem('municipios_local', JSON.stringify(municipios_local));
+            console.log(localStorage.getItem("departamentos_local"));
+            departamentos_local.forEach(element => {
+                $("#departamento2").append('<option value="' + element.Departamento + '">' + element.Departamento + '</option>')
+            });
+
+    },
+    error: function(response) {
+        console.log("Ocurrió un error al traer los municipios");
+    },
+});
+$("#departamento2").change(function(e){
+    //alert(e.target.value);
+
+    $("#municipio2").empty();
+    let munfiltro = JSON.parse(localStorage.getItem('municipios_local'));
+    let result = munfiltro.filter(m=>m.Departamento == e.target.value);
+
+    result.forEach(element => {
+                $("#municipio2").append('<option value="' + element.Municipio + '">' + element.Municipio + '</option>')
+            })
+
+
+});
+});
+  </script>
 </body>
 </html>
